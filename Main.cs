e7b0856a5e991f789ba34fe3f2c0750c6d163f2b
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2025-02-10 22:56:35 +0300

    2.0

--- Main.cs
+++ Main.cs
@@ -1,51 +1,137 @@
-﻿using CounterStrikeSharp.API.Core.Capabilities;
-using Hooks;
-using CounterStrikeSharp.API;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Entities;
-using Microsoft.Extensions.Logging;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Modules.Admin;
+using System.Text.Json.Serialization;
+using HGR;
 
+namespace EveryoneHGR;
 
-namespace EndRoundHooks;
-public class EndRoundHooks : BasePlugin
+public class PluginConfig : BasePluginConfig
 {
+    [JsonPropertyName("RoundStart")] public HGRInfo RoundStart { get; set; } = new HGRInfo(1,2,1);
+    [JsonPropertyName("RoundEnd")] public HGRInfo RoundEnd { get; set; } = new HGRInfo(-1,-1,-1);
+}
 
+public class AdminHGR : BasePlugin, IPluginConfig<PluginConfig>
+{
+    public PluginConfig Config { get; set; }
+    public override string ModuleName => "EveryoneHGR";
+    public override string ModuleVersion => "2.0";
     public override string ModuleAuthor => "Nick Fox";
-    public override string ModuleName => "EndRound Hooks";
-    public override string ModuleVersion => "1.0";
 
-    private bool round_end;
-
-    private IHooksApi? _hooks;
-    private PluginCapability<IHooksApi> PluginHooks { get; } = new("hooks:nfcore");
-
-    public override void OnAllPluginsLoaded(bool hotReload)
+    private IHGRApi? _hgr;
+    private PluginCapability<IHGRApi> PluginHooks { get; } = new("hgr:nfcore");
+    public void OnConfigParsed(PluginConfig config)
     {
-        _hooks = PluginHooks.Get();
-
-        if (_hooks == null) return;
-
-
-        _hooks.AddHook((playerinfo) => {
-
-            if (round_end)
-                playerinfo.Set(HookState.Enabled);
-        });
-
-        RegisterEventHandler<EventRoundEnd>((@event, info) =>
-        {            
-            round_end = true;
-            return HookResult.Continue;
-        });
-
-
-        RegisterEventHandler<EventRoundStart>((@event, info) =>
-        {
-            round_end = false;
-            return HookResult.Continue;
-        });
+        Config = config;
     }
 
 
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        _hgr = PluginHooks.Get();
+
+        hgrCount = new int[3][];
+        for (int i = 0; i < 3; i++)
+            hgrCount[i] = new int[65];
+
+        if (_hgr == null) return;
+        _hgr.AddHook(HGRHook, 100);
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        _hgr.RemHook(HGRHook);
+    }
+
+
+    private int[][] hgrCount; // 0 - Hooks, 1 - Grabs, 2 - Ropes
+
+
+    private void HGRHook(PlayerHGR info)
+    {
+        if (info.State() == HGRState.Disabled)
+        {
+            int i = 0;
+            switch (info.Mode())
+            {
+                case HGRMode.Hook: i = 0; break;
+                case HGRMode.Grab: i = 1; break;
+                case HGRMode.Rope: i = 2; break;
+                default: return;
+            }
+
+            if (hgrCount[i][info.Player().Slot] == -1)
+                info.Enable();
+            else
+                if (hgrCount[i][info.Player().Slot] > 0)
+            {
+                hgrCount[i][info.Player().Slot]--;
+                info.Enable();
+
+                if (hgrCount[i][info.Player().Slot] == 0)
+                    info.Player().PrintToChat(Localizer["ev_hgr.expired"]);
+                else
+                    info.Player().PrintToChat(String.Format(Localizer["ev_hgr.use_count"], hgrCount[i][info.Player().Slot]));
+            }
+        }
+    }
+
+    [GameEventHandler]
+    public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 65; j++)
+                hgrCount[i][j] = 0;
+        return HookResult.Continue;
+    }
+
+
+    [GameEventHandler]
+    public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
+    {
+        OnRoundEvent(true);
+        return HookResult.Continue;
+    }
+
+
+    [GameEventHandler]
+    public HookResult OnFreezeEnd(EventRoundFreezeEnd @event, GameEventInfo info)
+    {
+        OnRoundEvent(false);
+        return HookResult.Continue;
+    }
+
+    private void OnRoundEvent(bool endRound)
+    {
+        HGRInfo info;
+        if (endRound)
+            info = Config.RoundEnd;
+        else
+            info = Config.RoundStart;
+
+        foreach (var player in Utilities.GetPlayers())
+        {
+            var slot = player.Slot;            
+
+            AddHGRValues(slot, 0, info.Hooks);
+            AddHGRValues(slot, 1, info.Grabs);
+            AddHGRValues(slot, 2, info.Ropes);
+
+        }
+    }
+    
+    
+
+    private void AddHGRValues(int slot, int index, int value)
+    {
+        if (value == -1)
+            hgrCount[index][slot] = -1;
+        else if (hgrCount[index][slot] != -1)
+            hgrCount[index][slot] += value;
+    }
 
 }
+
---
+++ Structs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryoneHGR;
+
+public class HGRInfo
+{
+    public int Hooks { get; set; }
+    public int Grabs { get; set; }
+    public int Ropes { get; set; }
+
+    public HGRInfo(int Hooks, int Grabs, int Ropes)
+    {
+        this.Hooks = Hooks;
+        this.Grabs = Grabs;
+        this.Ropes = Ropes;
+    }
+}
